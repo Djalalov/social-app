eac8b47169781fa125470645dc89958d817fee8a
Djalalov <edjalalov@outlook.com>
2022-06-03 16:44:55 +0500

    .updates

--- pages/[id].js
+++ pages/[id].js
@@ -61,7 +61,7 @@ const PostPage = ({ trendingResults, followResults, providers }) => {
 			</Head>
 			<main className="min-h-screen flex max-w-[1500px] mx-auto">
 				<Sidebar />
-				<div className="bg-orange-500 flex-grow border-l border-r border-gray-700 max-w-2xl sm:ml-[73px] xl:ml-[370px]">
+				<div className="flex-grow border-l border-r border-gray-700 max-w-2xl sm:ml-[73px] xl:ml-[370px]">
 					<div className="flex items-center px-1.5 py-2 border-b border-gray-700 text-[#d9d9d9] font-semibold text-xl gap-x-4 sticky top-0 z-50 bg-black">
 						<div
 							className="hoverAnimation w-9 h-9 flex items-center justify-center xl:px-0"
