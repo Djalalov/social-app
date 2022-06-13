88c56bceeb99e65a8636218844d3c3d3e37d9d10
Djalalov <edjalalov@outlook.com>
2022-06-13 23:26:33 +0500

    .

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -24,7 +24,7 @@ const Sidebar = () => {
 	return (
 		<div className="hidden sm:flex flex-col items-center xl:items-start xl:w-[340px] p-2 fixed h-full">
 			<div className="flex items-center justify-center w-14 h-14 hoverAnimation p-0 xl:ml-24">
-				<Image alt="logo" src={logo} width={40} height={40} />
+				<Image alt="logo" src={logo} width={70} height={70} />
 			</div>
 
 			<div className="space-y-2.5 mt-4 mb-2.5 xl:ml-24">
